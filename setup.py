9389c971c726eeedbf1b3c76e39ab62e22fbce1f
Varun Ramesh <varun@gridspace.com>
2023-09-05 11:51:24 -0700

    Tests seem to be running

--- setup.py
+++ setup.py
@@ -13,190 +13,62 @@ from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 from Cython.Build import cythonize
 
-
 OPENFST_VERSION = "1.7.3"
-
-
-def copy(src, dst):
-    print("copying {} -> {}".format(src, dst))
-    shutil.copy(src, dst)
-
-
-def get_file_sha256(filename):
-    hasher = hashlib.md5()
-    with open(filename, "rb") as afile:
-        buf = afile.read()
-        hasher.update(buf)
-    return hasher.hexdigest()
-
-
-def get_filename_with_sha256(filename):
-    sha256 = get_file_sha256(filename)
-    basename = os.path.basename(filename)
-    basename_parts = basename.split(".")
-    libname, libext = basename_parts[0], ".".join(basename_parts[1:])
-    return "%s-%s.%s" % (libname, sha256[:8], libext)
-
-
-class OpenFstExtension(Extension):
-    def __init__(self):
-        Extension.__init__(self, name="openfst_python.pywrapfst", sources=[])
-
-
-class OpenFstBuild(build):
-
-    user_options = build.user_options + [
-        (
-            "download-dir=",
-            None,
-            "directory containing the openfst-%s.tar.gz file" % OPENFST_VERSION,
-        )
-    ]
-
-    @property
-    def openfst_basename(self):
-        return "openfst-%s.tar.gz" % OPENFST_VERSION
-
-    def initialize_options(self):
-        build.initialize_options(self)
-        self.download_dir = None
-
-    def finalize_options(self):
-        build.finalize_options(self)
-        # self.set_undefined_options("build_ext", ("download_dir", "download_dir"))
-        if self.download_dir:
-            openfst_tar_gz = os.path.join(self.download_dir, self.openfst_basename)
-            assert os.path.isfile(openfst_tar_gz), (
-                "File %s does not exist" % openfst_tar_gz
-            )
-        else:
-            self.download_dir = self.build_temp
-
+OPENFST_DIR = f"./openfst-{OPENFST_VERSION}"
+OPENFST_ARCHIVE = f"openfst-{OPENFST_VERSION}.tar.gz"
+OPENFST_URL = f"http://www.openfst.org/twiki/pub/FST/FstDownload/{OPENFST_ARCHIVE}"
 
 class OpenFstBuildExt(build_ext):
-    @property
-    def openfst_dirname(self):
-        return "./openfst-%s" % (OPENFST_VERSION)
-
-    @property
-    def openfst_filename(self):
-        return os.path.join(self.download_dir, self.openfst_basename)
-
-
-    @property
-    def openfst_deps_libs(self):
-        return [
-            "%s/src/extensions/far/.libs/libfstfar.so" % self.openfst_dirname,
-            "%s/src/extensions/far/.libs/libfstfarscript.so" % self.openfst_dirname,
-            "%s/src/script/.libs/libfstscript.so" % self.openfst_dirname,
-            "%s/src/lib/.libs/libfst.so" % self.openfst_dirname,
-        ]
-
-    @property
-    def output_dir(self):
-        return "%s/openfst_python" % self.build_lib
-
-    def initialize_options(self):
-        build_ext.initialize_options(self)
-        self.download_dir = None
-
-    def finalize_options(self):
-        build_ext.finalize_options(self)
-        self.set_undefined_options("build", ("download_dir", "download_dir"))
-
-    def openfst_download(self):
-        if os.path.exists(self.openfst_dirname):
+    def openfst_download_and_extract(self):
+        # Skip if OpenFST dir already exists.
+        if os.path.exists(OPENFST_DIR):
             return
 
-        if not os.path.isdir(self.build_temp):
-            os.makedirs(self.build_temp)
-
-        if not os.path.exists(self.openfst_filename):
-            print("downloading from %s" % self.openfst_url)
-            r = requests.get(self.openfst_url, verify=False, stream=True)
+        # Download OpenFST archive.
+        if not os.path.exists(OPENFST_ARCHIVE):
+            print("Downloading from %s" % OPENFST_URL)
+            r = requests.get(OPENFST_URL, verify=False, stream=True)
             r.raw.decode_content = True
-            with open(self.openfst_filename, "wb") as f:
+            with open(OPENFST_ARCHIVE, "wb") as f:
                 shutil.copyfileobj(r.raw, f)
 
-    def openfst_extract(self):
-        if not os.path.exists(self.openfst_dirname):
-            extract_cmd = ["tar", "xzf", self.openfst_filename, "-C", self.build_temp]
-            subprocess.check_call(extract_cmd)
+        subprocess.check_call(["tar", "xzf", OPENFST_ARCHIVE])
 
     def openfst_configure_and_make(self):
-        if not os.path.exists(self.openfst_main_lib):
-            old_dir = os.getcwd()
-            os.chdir(self.openfst_dirname)
-            configure_cmd = [
-                "./configure",
-                "--enable-compact-fsts",
-                "--enable-compress",
-                "--enable-const-fsts",
-                "--enable-far",
-                "--enable-linear-fsts",
-                "--enable-lookahead-fsts",
-                "--enable-special",
-            ]
-            subprocess.check_call(configure_cmd)
-            subprocess.check_call(["make", "-j8"])
-            os.chdir(old_dir)
+        old_dir = os.getcwd()
+        os.chdir(OPENFST_DIR)
+        subprocess.check_call([
+            "./configure",
+            "--enable-compact-fsts",
+            "--enable-compress",
+            "--enable-const-fsts",
+            "--enable-far",
+            "--enable-linear-fsts",
+            "--enable-lookahead-fsts",
+            "--enable-special",
+        ])
+        subprocess.check_call(["make", "-j8"])
+        os.chdir(old_dir)
 
     def openfst_copy_libraries(self):
-        for src in self.openfst_deps_libs:
-            dst = "%s/%s" % (self.output_dir, get_filename_with_sha256(src))
-            copy(src, dst)
+        libraries = [
+            "src/extensions/far/.libs/libfstfar.so.17",
+            "src/extensions/far/.libs/libfstfarscript.so.17",
+            "src/script/.libs/libfstscript.so.17",
+            "src/lib/.libs/libfst.so.17",
+        ]
+        destination = "./openfst_python/lib"
+        if not os.path.isdir(destination):
+            os.mkdir(destination)
 
-    def openfst_fix_libraries(self):
-        def patchelf_needed(so_filename):
-            patchelf_cmd = ("patchelf", "--print-needed", so_filename)
-            output = subprocess.check_output(patchelf_cmd)
-            if isinstance(output, bytes):
-                return output.decode("utf-8").split()
-            else:
-                return output.split()
-
-        def patchelf_replace(so_filename, oldso, newso):
-            patchelf_cmd = ("patchelf", "--replace-needed", oldso, newso, so_filename)
-            subprocess.check_call(patchelf_cmd)
-
-        def patchelf_remove(so_filename, oldso):
-            patchelf_cmd = ("patchelf", "--remove-needed", oldso, so_filename)
-            subprocess.check_call(patchelf_cmd)
-
-        def patchelf_rpath_origin(so_filename):
-            patchelf_cmd = ("patchelf", "--set-rpath", "$ORIGIN", so_filename)
-            subprocess.check_call(patchelf_cmd)
-
-        def patchelf_replace_all(so_filename, so_map):
-            for oldso in patchelf_needed(so_filename):
-                oldso_noversion = re.sub(r"^(.*\.so)\.[0-9]+$", r"\1", oldso)
-                if oldso_noversion in so_map:
-                    newso = so_map[oldso_noversion]
-                    patchelf_replace(so_filename, oldso, newso)
-
-        def patchelf_remove_libpython(so_filename):
-            for oldso in patchelf_needed(so_filename):
-                if re.match(r"^libpython.*$", oldso) is not None:
-                    patchelf_remove(so_filename, oldso)
-
-        somap = {
-            os.path.basename(dep): get_filename_with_sha256(dep)
-            for dep in self.openfst_deps_libs
-        }
-
-        for sofile in os.listdir(self.output_dir):
-            if re.match(r"^.*(\.so).*$", sofile) is not None:
-                sofile = os.path.join(self.output_dir, sofile)
-                patchelf_replace_all(sofile, somap)
-                patchelf_remove_libpython(sofile)
-                patchelf_rpath_origin(sofile)
+        for library in libraries:
+            shutil.copy(os.path.join(OPENFST_DIR, library), destination)
 
     def run(self):
-        super().run()
+        self.openfst_download_and_extract()
         self.openfst_configure_and_make()
         self.openfst_copy_libraries()
-        self.openfst_fix_libraries()
-
+        super().run()
 
 with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as fh:
     long_description = fh.read()
@@ -216,18 +88,14 @@ setup(
         name="openfst_python.pywrapfst",
         sources=[os.path.join("openfst-1.7.3", "src/extensions/python/pywrapfst.cc")],
         include_dirs=[os.path.join("openfst-1.7.3", "src/include/")],
-        library_dirs=[
-            os.path.join("openfst-1.7.3", "src/lib/.libs/"),
-            os.path.join("openfst-1.7.3", "src/extensions/far/.libs/"),
-            os.path.join("openfst-1.7.3", "src/script/.libs/"),
-        ],
+        library_dirs=["openfst_python/lib"],
         libraries=["fst", "fstscript", "fstfar", "fstfarscript"],
         extra_link_args = ["-Wl,-rpath=$ORIGIN/lib/."],
     )],
     package_data={
         'openfst_python': ['lib/*']
     },
-    # cmdclass=dict(build=OpenFstBuild, build_ext=OpenFstBuildExt),
+    cmdclass=dict(build_ext=OpenFstBuildExt),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
