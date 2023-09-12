7a2d9e930b6ac9e681f10bc7353e7a057d17aaa8
Varun Ramesh <varun@gridspace.com>
2023-09-12 11:26:22 -0700

    Update build list

--- generate-index.py
+++ generate-index.py
@@ -9,14 +9,15 @@ with open("index.html", "w") as output:
     output.write("""<!DOCTYPE html>
 <html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>
-<body>""")
+<body>
+""")
                  
     for release in releases:
         for asset in release["assets"]:
             url = asset["browser_download_url"]
             name = asset["name"]
             if url.endswith(".whl"):
-                output.write(f"<a href='{url}'>{name}</a><br>")
+                output.write(f"<a href='{url}'>{name}</a><br>\n")
 
     output.write("""</body>
 </html>""")
