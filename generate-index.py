256034e82699dcee62c65396e7efc67a9ff8bcfa
Varun Ramesh <varunramesh@live.com>
2023-09-29 17:46:09 -0700

    Update index

--- generate-index.py
+++ generate-index.py
@@ -13,11 +13,14 @@ with open("index.html", "w") as output:
 """)
                  
     for release in releases:
+        output.write(f"<h1>{release['tag_name']}</h1>\n")
+        output.write(f"<ul>\n")
         for asset in release["assets"]:
             url = asset["browser_download_url"]
             name = asset["name"]
             if url.endswith(".whl"):
-                output.write(f"<a href='{url}'>{name}</a><br>\n")
+                output.write(f"<li><a href='{url}'>{name}</a></li>\n")
+        output.write(f"</ul>\n")
 
     output.write("""</body>
 </html>""")
